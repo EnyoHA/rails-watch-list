197c46540d5f1f191d45041885cc6dee5536ea14
Enyo HumphreyAckumey <enyomha@gmail.com>
2023-02-24 12:52:19 +0000

    um I dont get heroku

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,7 +1,9 @@
 class List < ApplicationRecord
   has_many :bookmarks
   has_many :movies, through: :bookmarks, dependent: :delete_all
+  has_one_attached :photo
 
   validates :name, presence: true
   validates :name, uniqueness: true
+  validates :photo, presence: true
 end
