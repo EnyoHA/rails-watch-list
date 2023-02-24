c103f1ff769f66d188dd007565ab929056e077f2
Enyo HumphreyAckumey <enyomha@gmail.com>
2023-02-24 15:08:38 +0000

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,30 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'rest-client'
+
 Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
 Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
 Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
 Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
+
+def api_key
+  ENV["API_KEY"]
+end
+
+def film_dataset
+  api_data = { key: api_key }
+  films = RestClient.get("http://tmdb.lewagon.com/movie/top_rated?api_key=#{api_data[:key]}")
+  films_array = JSON.parse(films)["results"]
+  # puts films_array
+  films_array.each do |film|
+    Movie.create(
+      title: film["title"],
+      # genre: film["genres"][0]["name"]
+      overview: film["overview"],
+      poster_url: film["poster_path"]
+    )
+  end
+end
+
+film_dataset()
